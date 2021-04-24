2e95b6972ecb401430ff0a024118c084996c2424
Ewhan Han <ewhan.han@gmail.com>
2021-04-24 11:26:41 -0400

    Cleanup + navbar + layouts

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,12 @@
 import * as React from "react"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layouts from "../components/Layouts"
 
 const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
+  <Layouts>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </Layouts>
 )
 
 export default NotFoundPage
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layouts from "../components/Layouts"
+
+const about = () => {
+  return (
+    <Layouts>
+      <h2>testing from about page</h2>
+    </Layouts>
+  )
+}
+
+export default about

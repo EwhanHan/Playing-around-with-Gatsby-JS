1f93f2ac7c119a92475e7c07fe0a27c77ab740b0
Ewhan Han <ewhan.han@gmail.com>
2021-04-24 23:57:08 -0400

    Gallery with query

---
+++ src/pages/gallery.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+
+const Gallery = () => {
+  const data = useStaticQuery(query);
+  const nodes = data.allFile.nodes;
+  return (
+    <Layout>
+      <main className='page'>
+        <Wrapper>
+          {nodes.map((imageData, index) => {
+            const { name } = imageData;
+            return (
+              <article key={index} className='gallery-item'>
+                <GatsbyImage
+                  image={imageData.childrenImageSharp[0].gatsbyImageData}
+                  alt={name}
+                  className='gallery-img'
+                />
+                <p>{name}</p>
+              </article>
+            );
+          })}
+        </Wrapper>
+      </main>
+    </Layout>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+
+  .gallery-item {
+    margin-right: 1rem;
+    text-align: center;
+    text-transform: capitalize;
+  }
+
+  .gallery-img {
+    border-radius: 0.5rem;
+  }
+`;
+
+const query = graphql`
+  {
+    allFile(
+      filter: { sourceInstanceName: { eq: "images" }, extension: { ne: "svg" } }
+    ) {
+      nodes {
+        name
+        childrenImageSharp {
+          gatsbyImageData(
+            layout: FIXED
+            placeholder: BLURRED
+            width: 200
+            height: 200
+          )
+        }
+        sourceInstanceName
+      }
+    }
+  }
+`;
+export default Gallery;

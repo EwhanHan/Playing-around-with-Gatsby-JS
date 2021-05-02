f56b8d03bdef7f6b01eef3ea116b4d699e7335e7
Ewhan Han <ewhan.han@gmail.com>
2021-05-02 09:17:42 -0400

    Integrate Formspree for contacts page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,7 +19,11 @@ const contact = () => {
             </p>
           </article>
           <article>
-            <form className='form contact-form'>
+            <form
+              className='form contact-form'
+              action='https://formspree.io/f/xzbydvja'
+              method='POST'
+            >
               <div className='form-row'>
                 <label htmlFor='name'>your name</label>
                 <input type='text' name='name' id='name' />
